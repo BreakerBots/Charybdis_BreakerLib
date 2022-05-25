fd210464fe1a5e18083f3bf42c94b99f9d8a7fcc
Roman-Tech-Plus <rrcabrahamson@gmail.com>
2022-05-25 16:37:55 -0700

    Update BreakerInterpolateingTreeMap.java

--- src/main/java/frc/robot/BreakerLib/util/math/interpolation/BreakerInterpolateingTreeMap.java
+++ src/main/java/frc/robot/BreakerLib/util/math/interpolation/BreakerInterpolateingTreeMap.java
@@ -5,14 +5,20 @@
 package frc.robot.BreakerLib.util.math.interpolation;
 
 import java.util.Map.Entry;
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import frc.robot.BreakerLib.util.math.interpolation.BreakerInterpolateable;
 
 /** Add your docs here. */
-public class BreakerInterpolateingTreeMap<K, V extends BreakerInterpolateable<V>> {
+public class BreakerInterpolateingTreeMap<K, V extends BreakerInterpolateable<V>> extends java.util.AbstractMap<K, V>  {
     private TreeMap<K, V> indexesAndValues;
     public BreakerInterpolateingTreeMap(TreeMap<K, V> indexesAndValues) {
         this.indexesAndValues = indexesAndValues;
+    }
+
+    public BreakerInterpolateingTreeMap() {
+        indexesAndValues = new TreeMap<K, V>();
     }
 
     public V getInterpolatedValue(K interpolendValue) {
@@ -30,5 +36,35 @@ public class BreakerInterpolateingTreeMap<K, V extends BreakerInterpolateable<V>
         }
 
         return high.getValue().interpolate((double) interpolendValue, (double) high.getKey(), high.getValue().getSelf(), (double) low.getKey(), low.getValue().getSelf());
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return indexesAndValues.entrySet();
+    }
+
+    @Override
+    public V put(K key, V value) {
+        return indexesAndValues.put(key, value);
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        indexesAndValues.putAll(m);
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return indexesAndValues.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return indexesAndValues.containsValue(value);
+    }
+
+    @Override
+    public boolean replace(K key, V oldValue, V newValue) {
+        return indexesAndValues.replace(key, oldValue, newValue);
     }
 }
