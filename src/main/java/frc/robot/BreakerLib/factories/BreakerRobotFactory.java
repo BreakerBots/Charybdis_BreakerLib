ba8dfa9b10c1679c8646652dd9459f4ec67fb81d
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-07-27 20:12:01 -0700

    Miscellaneous robot work

---
+++ src/main/java/frc/robot/BreakerLib/factories/BreakerRobotFactory.java
@@ -0,0 +1,8 @@
+package frc.robot.BreakerLib.factories;
+
+/** Factory for setting up subsystems and command assignments. */
+public class BreakerRobotFactory {
+
+    // I want to find a way to quickly, easily enable/disable subsystems and commands.
+    // I am still looking.
+}
