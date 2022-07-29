231432d4ea4c973b20263663a20f6a72b117ea8b
YousifAlkhalaf <58121901+YousifAlkhalaf@users.noreply.github.com>
2022-07-28 22:03:46 -0700

    Still trying to make my factory work.

--- src/main/java/frc/robot/BreakerLib/factories/BreakerRobotFactory.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.BreakerLib.factories;
-
-/** Factory for setting up subsystems and command assignments. */
-public class BreakerRobotFactory {
-
-    // I want to find a way to quickly, easily enable/disable subsystems and commands.
-    // I am still looking.
-}
---
+++ src/main/java/frc/robot/BreakerLib/factories/BreakerSubsystemFactory.java
@@ -0,0 +1,21 @@
+package frc.robot.BreakerLib.factories;
+
+import java.util.Map;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Factory for setting up subsystems and command assignments. */
+public class BreakerSubsystemFactory {
+
+    public static <T extends SubsystemBase> T build(Map<String, Boolean> subsystemMap) {
+
+        return null;
+        // String type = obj.getClass().getSimpleName();
+
+        // for (String subsystemName: subsystemMap.keySet()) {
+        //     if () {
+
+        //     }
+        // }
+    }
+}
